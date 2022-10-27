266a9c0ae244dbd4a8cebad72e5dd3a81e729560
Asakundwi <asakundwi@pop-os.localdomain>
2022-10-27 04:36:55 +0200

    initial solution

---
+++ 383.py
@@ -0,0 +1,10 @@
+class Solution:
+	def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+		m = [i for i in magazine]
+		if len(magazine) < len(ransomNote):
+			return False
+		for i in ransomNote:
+			if i in m:
+				m.remove(i)
+			else:
+				return False
