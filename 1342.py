366dc5268f67166d2ddeccb4aecbb3dadf9f3316
Asakundwi <asakundwi@pop-os.localdomain>
2022-10-27 04:24:53 +0200

    initial solution

---
+++ 1342.py
@@ -0,0 +1,11 @@
+class Solution:
+	def numberOfSteps(self, num: int) -> int:
+		n = 0
+		while(num != 0):
+			if num % 2 == 0:
+				n += 1
+				num = num/2
+			else:
+				n += 1
+				num -= 1
+		return n
