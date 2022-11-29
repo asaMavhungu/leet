74b170b6777fb8c8c994b016d3429037bccf4b2b
Asakundwi Mavhungu <mavhasa17@gmail.com>
2022-11-29 16:07:24 +0200

    Better optimized question 20 for time and readability

--- python3/20.py
+++ python3/20.py
@@ -1,23 +1,19 @@
 class Solution:
 
-	def val(self, s: str) -> bool:
-		if len(s)%2 == 1:
-			return False
-		if len(s) == 2:
-			if ord(s[0]) - ord(s[-1]) not in [-1, -2]:
-				return False
-			return True
-		for i in range(len(s)//2):
-			if ord(s[i]) - ord(s[i+1]) in [-1, -2]:
-				return self.val(s[:i] + s[i+2:])
-		return False
-
-	
 	def isValid(self, s: str) -> bool:
-		return self.val(s)
+		hash: dict = { '(':')', '[': ']', '{':'}' }
+		stack: list = []
+		for char in s:
+			if stack and char == stack[-1]:
+				stack.pop()
+			elif char in hash.keys():
+				stack.append(hash[char])
+			else:
+				return False
+		return stack == []
 	
 
 asa: Solution = Solution()
-s: str = "()\{\}}{"
-one: bool = asa.isValid(s)
-print(one)
+s: str = "]"
+
+print(asa.isValid(s))
