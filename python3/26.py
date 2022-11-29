652d389a2b9d711d9ab3ee851495ca2a26678bcc
Asakundwi Mavhungu <mavhasa17@gmail.com>
2022-11-30 01:43:11 +0200

    problem 26 completed

---
+++ python3/26.py
@@ -0,0 +1,17 @@
+from typing import List
+class Solution:
+	def removeDuplicates(self, nums: List[int]) -> int:
+		turtoise = hare = 1
+		asa = len(nums)
+		for i in range(1, asa):
+			if nums[i] == nums[turtoise-1]:
+				nums[i] = '_'
+
+			else:
+				nums[turtoise], nums[i] = nums[i], nums[turtoise]
+				turtoise += 1
+		return turtoise
+
+asa = Solution()
+k = asa.removeDuplicates([0,1,1,1,1,1,1,2,2,2,3,3,3,3,3,4])
+print(k)
