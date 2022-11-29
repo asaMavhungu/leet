d4ed90a0af0d640b9d8577878bfe2c43b9c82331
Asakundwi Mavhungu <mavhasa17@gmail.com>
2022-11-30 01:48:54 +0200

    problem 26 even further optimized

--- python3/26.py
+++ python3/26.py
@@ -1,9 +1,8 @@
 from typing import List
 class Solution:
 	def removeDuplicates(self, nums: List[int]) -> int:
-		turtoise = hare = 1
-		asa = len(nums)
-		for i in range(1, asa):
+		turtoise = 1
+		for i in range(1, len(nums)):
 			if nums[i] > nums[turtoise-1]:
 				nums[turtoise] = nums[i]
 				turtoise += 1
