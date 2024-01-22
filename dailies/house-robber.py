f887eee5ec0cd864f9c05264321e711d8b5a2446
Asakundwi Mavhungu <mavhasa17@gmail.com>
2024-01-22 20:54:21 +0200

    finished daily 22/01/24

---
+++ dailies/house-robber.py
@@ -0,0 +1,15 @@
+class Solution:
+	def rob(self, nums: List[int]) -> int:
+
+		if len(nums) == 1:
+			return nums[0]
+
+		if len(nums) < 3:
+			return max(nums[0], nums[1])
+		
+		nums[2] = nums[2] + nums[0]
+
+		for i in range(3, len(nums)):
+			nums[i] = nums[i] + max(nums[i-2], nums[i-3])
+
+		return max(nums[-1], nums[-2])
